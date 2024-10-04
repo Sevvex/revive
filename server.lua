329db7c6ac964874d860f432775782167a287b52
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-10-04 22:25:14 +0200

    Update and rename revive.lua to server.lua

--- server.lua
+++ server.lua
@@ -5,7 +5,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 local function isAdminOrHigher(xPlayer)
     local group = xPlayer.getGroup()
-    return group == 'admin' or group == 'superadmin' or group == 'projektleiter' or group == 'supporter' or group == 'moderator'
+    return group == 'admin' or group == 'superadmin' or group == 'projektleiter' or group == 'manager' or group == 'supporter' or group == 'moderator'
 end
 
 
@@ -21,6 +21,22 @@ RegisterCommand('revive', function(source, args, rawCommand)
         else
             TriggerClientEvent('esx:showNotification', source, 'Ungültige Spieler-ID.')
         end
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Du hast keine Berechtigung, diesen Befehl zu verwenden.')
+    end
+end, false)
+
+
+RegisterCommand('reviveall', function(source, args, rawCommand)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+
+    if isAdminOrHigher(xPlayer) then
+        
+        for _, playerId in ipairs(GetPlayers()) do
+            TriggerClientEvent('esx_ambulancejob:revive', playerId)
+        end
+        TriggerClientEvent('esx:showNotification', source, 'Alle Spieler wurden revived.')
     else
         TriggerClientEvent('esx:showNotification', source, 'Du hast keine Berechtigung, diesen Befehl zu verwenden.')
     end
