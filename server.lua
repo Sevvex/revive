d7521c1dd2707e16d1b8db1847a98e5e853b2138
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-10-05 01:04:00 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -14,22 +14,6 @@ local function isAdminOrHigher(playerId)
     return allowedGroups[playerGroup] or false
 end
 
-
-RegisterCommand('revive', function(source, args)
-    local targetId = tonumber(args[1])
-    if isAdminOrHigher(source) then
-        if targetId and GetPlayerName(targetId) then
-            TriggerClientEvent('revivePlayer', targetId)
-            print('Befehl /revive von ' .. GetPlayerName(source) .. ' für Spieler-ID ' .. targetId)
-        else
-            print('Ungültige Spieler-ID oder Spieler ist nicht online.')
-        end
-    else
-        print('Spieler hat keine Berechtigung für diesen Befehl.')
-    end
-end, false)
-
-
 RegisterCommand('reviveall', function(source)
     if isAdminOrHigher(source) then
         for _, playerId in ipairs(GetPlayers()) do
